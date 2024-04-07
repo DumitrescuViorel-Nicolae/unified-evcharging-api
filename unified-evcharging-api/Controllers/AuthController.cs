89d1ba75d96728b147c7e2e8c669fa7be5044e1d
Dumitrescu Viorel-Nicolae <nicolaedumitrescu.official@yahoo.com>
2024-04-07 18:22:07 +0300

    initialize backend

---
+++ unified-evcharging-api/Controllers/AuthController.cs
@@ -0,0 +1,27 @@
+﻿using Domain.DTOs;
+using Domain.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        //Dependency Injection
+
+        public AuthController()
+        {
+
+        }
+
+        [HttpPost("register")]
+        [AllowAnonymous]
+        public ActionResult<UserDTO> RegisterUser(UserDTO user)
+        {
+            return Ok(user);
+        }
+    }
+}
